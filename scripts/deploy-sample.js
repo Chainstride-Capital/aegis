77dd86a851169f6e25f107025bfb80a9c0c8c6eb
Chainstride Dev <admin@chainstride.capital>
2021-05-06 00:18:37 +0200

    Initial project structure

---
+++ scripts/deploy-sample.js
@@ -0,0 +1,13 @@
+
+const hre = require("hardhat");
+
+async function main() {
+
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
---
+++ test/aegis-impl.js
@@ -0,0 +1,5 @@
+const { expect } = require("chai");
+
+describe("Example Aegis-protected token sale", function() {
+
+});
