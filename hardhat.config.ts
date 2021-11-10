961cadcec7e4f320e0a28d767c41b71ae4fce0fc
Chainstride Dev <admin@chainstride.capital>
2021-11-10 16:47:27 +0100

    Format

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,16 +1,16 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomiclabs/hardhat-etherscan';
+import '@nomiclabs/hardhat-waffle';
+import '@typechain/hardhat';
+import 'hardhat-gas-reporter';
+import 'solidity-coverage';
 
 dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.6.12",
+  solidity: '0.6.12',
   networks: {
     hardhat: {
       forking: {
@@ -18,15 +18,15 @@ const config: HardhatUserConfig = {
         blockNumber: 13575777
       },
       allowUnlimitedContractSize: true
-    },
+    }
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
+    currency: 'USD'
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
 };
 
 export default config;
