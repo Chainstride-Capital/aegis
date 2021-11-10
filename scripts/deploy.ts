73bfed2a6243fb6b19350124ba7c0c6498bedaea
Chainstride Dev <admin@chainstride.capital>
2021-11-10 16:47:10 +0100

    Deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,29 +1,28 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat';
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const wethAddress = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
+  
+  const AegisERC20 = await ethers.getContractFactory('AegisERC20');
+  const aegisERC20 = await AegisERC20.deploy();
+  await aegisERC20.deployed();
+  console.log(`Example Aegis ERC20 deployed to ${aegisERC20.address}`);
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const SameBlockStrategy = await ethers.getContractFactory('AegisSameBlockStrategy');
+  const sameBlockStrategy = await SameBlockStrategy.deploy(true, 3600 * 24 * 90); // same block strategy should vest tokens over ~90 days
+  await sameBlockStrategy.deployed();
 
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  const AegisShield = await ethers.getContractFactory('AegisShield');
+  const aegisShield = await AegisShield.deploy(
+    [sameBlockStrategy.address], // strategies to use
+    aegisERC20.address, // address of the token we're protecting
+    wethAddress, // pair is with wrapped ether
+    0 // uniswap
+  );
+  await aegisShield.deployed();
+  console.log(`Aegis Shield deployed to ${aegisShield.address}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
